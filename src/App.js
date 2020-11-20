95d3b881fd4e444058c75c0cc24093a94fd6a234
rico.putra <rico.putra@outlook.co.id>
2020-11-20 10:21:21 +0700

    DONE sidebar layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Sidebar from './Sidebar.js';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <h1>TWITTER CLONE</h1>
+
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Feed */}
+
+      {/* Widget */}
     </div>
   );
 }
