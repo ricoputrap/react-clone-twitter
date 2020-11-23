68fa673a2e345e8378b5028b5303adab3902b4b1
rico.putra <rico.putra@outlook.co.id>
2020-11-23 09:28:54 +0700

    DONE itegrating react Posts data with realtime database using Firebase

--- src/Feed.js
+++ src/Feed.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import TweetBox from './TweetBox.js'
 import Post from './Post.js'
 import './Feed.css'
-
+import db from './firebase';
 
 function Feed() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        /**
+         * `posts` is a collection in our db
+         * `doc` is each document (or post) from `posts` collection
+         * `doc.data()` is the data (displayName, etc) of `doc`
+         */
+        db.collection('posts').onSnapshot(snapshot => (
+            setPosts(snapshot.docs.map(doc => doc.data()))
+        ))
+    }, []);
+
     return (
         <div className="feed">
             <div className="feed__header">
@@ -13,16 +26,26 @@ function Feed() {
             
             <TweetBox />
 
-            <Post
+            {posts.map(post => (
+                <Post
+                displayName={post.displayName}
+                username={post.username}
+                verified={post.verified}
+                text={post.text}
+                avatar={post.avatar}
+                image={post.image} />
+            ))}
+
+            {/* <Post
                 displayName="Rico Putra"
                 username="@rico_rpp21"
                 verified
                 text="TEST YOOO it's working!"
                 avatar="/static/hilih.png"
-                image="https://media.giphy.com/media/fuJjrm6Cv2onpmpPEK/giphy.gif" />
+                image="https://media.giphy.com/media/fuJjrm6Cv2onpmpPEK/giphy.gif" /> */}
+            {/* <Post />
             <Post />
-            <Post />
-            <Post />
+            <Post /> */}
         </div>
     )
 }
