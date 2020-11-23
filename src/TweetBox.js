d7bc773c3c3d9704c675caf4364f3535b4fedceb
rico.putra <rico.putra@outlook.co.id>
2020-11-23 09:46:23 +0700

    DONE add post using firestore

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,22 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TweetBox.css'
 import { Avatar, Button } from '@material-ui/core';
+import db from './firebase';
 
 function TweetBox() {
+    const [tweetMessage, setTweetMessage] = useState("");
+    const [tweetImage, setTweetImage] = useState("");
+
+    const sendTweet = e => {
+        e.preventDefault();
+
+        db.collection('posts').add({
+            displayName: 'Putra Rico',
+            username: "putrarico",
+            verified: true,
+            text: tweetMessage,
+            image: tweetImage,
+            avatar: 
+                "https://robohash.org/014795ea1a3b5c7fd6be33b2578a2c5b?set=set4&bgset=&size=400x400",
+        });
+
+        setTweetMessage("");
+        setTweetImage("");
+    }
+
     return (
         <div className="tweetBox">
             <form>
                 <div className="tweetBox__input">
                     <Avatar src="/static/hilih.png" />
                     <input 
+                        value={ tweetMessage }
+                        onChange={ e => setTweetMessage(e.target.value) }
                         placeholder="What's happening?" 
                         type="text" />
                 </div>
                 <input 
+                    value={ tweetImage }
+                    onChange={ e => setTweetImage(e.target.value) }
                     className="tweetBox__imageInput"
                     placeholder="Optional: Enter image URL" 
                     type="text" />
-                <Button className="tweetBox__tweetButton">Tweet</Button>
+                <Button
+                    onClick={ sendTweet }
+                    className="tweetBox__tweetButton">
+                    Tweet
+                </Button>
             </form>
         </div>
     )
